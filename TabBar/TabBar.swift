1e8288aba2c2db193600c0b15192e19d9d6bbdab
ᴀᴍ <hi@codeabyte.com>
2018-01-19 21:46:38 +0100

    Swift Tabbar without storyboards
    Swift Tabbar without storyboards

---
+++ TabBar/TabBar.swift
@@ -0,0 +1,63 @@
+//
+//  TabBar.swift
+//  TabBar
+//
+//  Created by Admir Mavric on 2018-01-18.
+//  Copyright © 2018 codeabyte. All rights reserved.
+//
+
+import UIKit
+
+
+class TabBar: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    class func customTabBar() -> UITabBarController {
+        
+        let homeVC : UIViewController = View1()
+        let settingsVC : UIViewController = View1()
+
+        
+        let homeNav : UINavigationController = TabNav.init(rootViewController: homeVC)
+        let settingsNav : UINavigationController = TabNav.init(rootViewController: settingsVC)
+
+        homeNav.tabBarItem = UITabBarItem.init(title: "Home", image: Public.getImage(imageName: "home"), selectedImage: Public.getImage(imageName: "home_on"))
+       
+        settingsNav.tabBarItem = UITabBarItem.init(title: "home2", image: Public.getImage(imageName: "home"), selectedImage: Public.getImage(imageName: "home_on"))
+     
+
+        let tc = UITabBarController()
+        tc.viewControllers = [homeNav,settingsNav];
+        
+        return tc
+        
+        
+        
+    }
+    
+    
+}
+
+class TabNav: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+class Public: NSObject {
+    class func getImage(imageName : NSString) -> UIImage {
+        let image : UIImage = UIImage.init(named: imageName as String)!
+        image.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        return image
+        
+    }
+}
+
+
+
---
+++ TabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by Admir Mavric on 2018-01-18.
+//  Copyright © 2018 codeabyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
